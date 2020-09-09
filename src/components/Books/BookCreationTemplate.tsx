60d671d5bdb02330ee24debc45795655d39d64ff
Emmanuel Xavier <emmanuelxavier040@gmail.com>
2020-09-10 01:37:48 +0530

    Card Features added
    1. Card operations added
    2. Card screens added
    3. Redux design implemented

---
+++ src/components/Books/BookCreationTemplate.tsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Box from '@material-ui/core/Box';
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+import AddIcon from '@material-ui/icons/Add';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 200,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  avatar: {
+    backgroundColor: red[500],
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+  cardHeaderRoot: {
+    overflow: "hidden"
+  },
+  cardHeaderContent: {
+    overflow: "hidden"
+  }
+
+}));
+
+export default function BookCreationTemplate(props: any) {
+  const [open, setOpen] = React.useState(false);
+  const [title, setTitle] = React.useState('');
+  const [url, setUrl] = React.useState('');
+
+  const classes = useStyles();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setTitle('')
+    setUrl('')
+  };
+
+  const handleCreate = () => {
+    if (title.length === 0 || url.length === 0) {        
+    } else {
+
+      const card = { title, url, groupId: props.groupId }
+      props.createCard(card)
+      handleClose()
+    }
+
+  }
+  return (
+    <div>
+      <Button variant="outlined" size='large' color="primary" onClick={handleClickOpen}>
+        Create Book
+      </Button>    {props.showLoading && <Box display='flex' justifyContent='center' padding={2}>
+        <CircularProgress />
+      </Box>
+      }
+
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" keepMounted={open} fullWidth={true}>
+        <DialogTitle id="form-dialog-title">Create Book</DialogTitle>
+        <DialogContent>
+          <Button component="label">
+            <Avatar aria-label="recipe" className={classes.avatar}>
+              <AddIcon />
+            </Avatar>
+            <input type="file" style={{ display: "none" }}/>
+          </Button>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Title"
+            type="text"
+            fullWidth
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            autoComplete={'off'}
+          />
+          <div><br /></div>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="URL"
+            type="url"
+            fullWidth
+            value={url}
+            onChange={(event) => setUrl(event.target.value)}
+            autoComplete={'off'}
+          /> <div><br /></div>
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} size='large' color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleCreate} size='large' color="primary">
+            Create
+          </Button>
+        </DialogActions>
+        <div><br /></div>
+      </Dialog>
+    </div>
+  );
+}
